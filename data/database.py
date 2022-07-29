3e6f5bae694ab181ee467f5c8604c496c0f9f60f
m.dehghankar <mohsendehghankar@gmail.com>
2022-07-29 19:51:42 +0430

    Add implementation of database query execution

--- data/database.py
+++ data/database.py
@@ -1,21 +1,37 @@
+import psycopg2
+
 """
 Utils for database connections
 """
 
 
 class Database:
-    def __init__(
-        self,
-        host: str,
-        username: str,
-        password: str,
-        dbname: str
-    ) -> None:
+    def __init__(self, host: str, username: str, password: str, dbname: str) -> None:
         self.host = host
         self.username = username
         self.password = password
         self.connection = None
 
     def connect(self):
-        # Connect to database based on credentials
-        pass
+        try:
+            self.connection = psycopg2.connect(
+                self.host,
+                self.dbname,
+                self.username,
+                self.password,
+            )
+        except Exception as e:
+            self.connection = None
+            print(f"Database connection error {e}")
+
+    def execute_query(self, query: str):
+        if self.connection == None:
+            raise Exception("Database is not connected")
+
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+            return cursor.fetchall()
+        except Exception as e:
+            print(f"Error running query {e}")
+            return None
