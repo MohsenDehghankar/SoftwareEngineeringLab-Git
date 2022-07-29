8530b93ad9967180bdbd3cf4c5521b4543e512be
m.dehghankar <mohsendehghankar@gmail.com>
2022-07-29 20:25:24 +0430

    Create and add test templates for summation logic

---
+++ tests/view_test.py
@@ -0,0 +1,9 @@
+class ViewTest:
+    def test_sumOfTwoNumbers_returnsCorrectResult(self):
+        pass
+
+    def test_sumOfThreeNumbers_returnsCorrectResult(self):
+        pass
+
+    def test_sumOfOneNumber_returnsTheNumberItSelf(self):
+        pass
