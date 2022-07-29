9c5b76551f4018c591a544dde3272a60efc5e94f
m.dehghankar <mohsendehghankar@gmail.com>
2022-07-29 20:04:50 +0430

    Implement summation logic

--- presentation/view.py
+++ presentation/view.py
@@ -5,14 +5,16 @@ APIs are implemented here
 from typing import Dict
 from domain.controller import Controller
 from presentation.request import Request
+from presentation.response import Response
 
 
 class View:
     def __init__(self, controller: Controller) -> None:
         self.controller = controller
 
-    def calculate_sum(request: Request) -> Dict:
+    def calculate_sum(self, request: Request) -> Response:
         """
         Calculates sum of number
         """
-        pass
+        summation = self.controller.calculate_sum(*request.data)
+        return Response(result=summation)
