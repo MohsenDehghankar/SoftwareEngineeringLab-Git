537c5c2b1d628902f008be905157232ec0205fcf
m.dehghankar <mohsendehghankar@gmail.com>
2022-07-29 19:41:51 +0430

    Implement database connection class

--- data/database.py
+++ data/database.py
@@ -1,3 +1,20 @@
 """
 Utils for database connections
 """
+
+
+class Database:
+    def __init__(
+        self,
+        host: str,
+        username: str,
+        password: str,
+    ) -> None:
+        self.host = host
+        self.username = username
+        self.password = password
+        self.connection = None
+
+    def connect(self):
+        # Connect to database based on credentials
+        pass
