97b2615b522730346d3b72014befdc31f56cbb28
m.dehghankar <mohsendehghankar@gmail.com>
2022-07-29 20:02:36 +0430

    Initialize request and response classes for view

--- presentation/view.py
+++ presentation/view.py
@@ -1,3 +1,18 @@
 """
 APIs are implemented here
 """
+
+from typing import Dict
+from domain.controller import Controller
+from presentation.request import Request
+
+
+class View:
+    def __init__(self, controller: Controller) -> None:
+        self.controller = controller
+
+    def calculate_sum(request: Request) -> Dict:
+        """
+        Calculates sum of number
+        """
+        pass
